18973b99f822a486007b225d47c0e614cd409979
Ken Reed <kenakioreed@gmail.com>
2018-02-15 20:12:37 -0500

    initial

--- src/lib.rs
+++ src/lib.rs
@@ -45,11 +45,14 @@ void main() {
 
 static FS_SRC: &'static str = "
 #version 300 es
+
 precision mediump float;
+uniform vec3 bcol;
 out vec4 out_color;
 
+
 void main() {
-    out_color = vec4(1.0, 1.0, 1.0, 1.0);
+    out_color = vec4(bcol, 1.0);
 }";
 
 
@@ -131,8 +134,8 @@ fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
 pub struct GlSys{
     length:usize,
     gl_window:glutin::GlWindow,
-    cs:ContextSetup
- 
+    cs:ContextSetup,
+    back_col:[f32;3]
 }
 
 
@@ -144,7 +147,7 @@ impl GlSysBuilder{
     pub fn new(events_loop:&glutin::EventsLoop)->GlSysBuilder{
         use glutin::GlContext;
 
-        let window = glutin::WindowBuilder::new().with_multitouch();
+        let window = glutin::WindowBuilder::new().with_multitouch()/*.with_dimensions(800,400)*/;
  
         let context = glutin::ContextBuilder::new()
         //we are targeting only opengl 3.0 es. and glsl 300 es.
@@ -153,11 +156,12 @@ impl GlSysBuilder{
         
         let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
 
-        println!("gl_window dim={:?}",gl_window.get_inner_size().unwrap());
+        //println!("gl_window dim={:?}",gl_window.get_inner_size().unwrap());
 
         let (width,height)=gl_window.get_inner_size().unwrap();
         GlSysBuilder{gl_window}
     }
+
     pub fn get_dim(&self)->(usize,usize){
         let (width,height)=self.gl_window.get_inner_size().unwrap();
         (width as usize,height as usize)
@@ -200,7 +204,7 @@ impl ContextSetup{
         let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
         let program = link_program(vs, fs);
 
-        println!("created vertex program");
+        //println!("created vertex program");
         let mut vao = 0;
         let mut vbo = 0;
 
@@ -219,20 +223,20 @@ impl ContextSetup{
                 gl::DYNAMIC_DRAW,
             );
 
-            println!("created buffer draw");
+            //println!("created buffer draw");
 
             // Use shader program
             gl::UseProgram(program);
-            println!("used program");
+            //println!("used program");
             //gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
             gl::BindAttribLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
             
-            println!("bind frag");
+            //println!("bind frag");
             // Specify the layout of the vertex data
             let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
-            println!("attrib location");
+            //println!("attrib location");
             gl::EnableVertexAttribArray(pos_attr as GLuint);
-            println!("enabled");
+            //println!("enabled");
             gl::VertexAttribPointer(
                 pos_attr as GLuint,
                 2,
@@ -242,7 +246,7 @@ impl ContextSetup{
                 ptr::null(),
             );
 
-            println!("creaed attrib");
+            //println!("creaed attrib");
         }
         let scalex = 2.0 * (1.0 / width as f32); //TODO don't hard code!!!!
         let scaley = 2.0 * (1.0 / height as f32);
@@ -259,13 +263,30 @@ impl ContextSetup{
             
             
             let myLoc:GLint = gl::GetUniformLocation(program, CString::new("mmatrix").unwrap().as_ptr());
-            println!("got uniform");
+            //println!("got uniform");
             gl::UniformMatrix4fv(myLoc, 1, 0,std::mem::transmute(&matrix[0][0]));
-            println!("set uniform matrix");
+            //println!("set uniform matrix");
         }
+        /*
+        unsafe{
+            let myLoc:GLint = gl::GetUniformLocation(program, CString::new("bcol").unwrap().as_ptr());
+      
+            let mut arr=[1.0,0.5,1.0f32];
+            gl::Uniform3fv(myLoc,1,std::mem::transmute(&arr[0]));
+            
+        }
+        */
+        /*
+        unsafe{
+            gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+            gl::Enable( gl::BLEND );
+        }
+        */
+        
 
         ContextSetup{fs,vs,vbo,vao,program}
     }
+
 }
 impl GlSys{
 
@@ -276,7 +297,7 @@ impl GlSys{
     ///0,height is bottom left
     ///width,height is bottom right
     pub fn new(builder:GlSysBuilder,verts:&[Vertex])->GlSys{
-        println!("verts len is ={}",verts.len());
+        //println!("verts len is ={}",verts.len());
         use glutin::GlContext;
         let GlSysBuilder{gl_window}=builder;
         let (width,height)=gl_window.get_inner_size().unwrap();
@@ -287,11 +308,24 @@ impl GlSys{
         let cs=ContextSetup::new(&gl_window,width,height,verts);
 
         //Self::update_uniform(program,&gl_window,width,height);
-        println!("updated uniform");
-        GlSys{gl_window,length:verts.len(),cs}
+        //println!("updated uniform");
+        GlSys{gl_window,length:verts.len(),cs,back_col:[0.2,0.2,0.2]}
 
     }
     
+
+    pub fn set_bot_color(&mut self,col:[f32;3]){
+        unsafe{
+            let myLoc:GLint = gl::GetUniformLocation(self.cs.program, CString::new("bcol").unwrap().as_ptr());
+      
+            //let mut arr=[1.0,0.5,1.0f32];
+            gl::Uniform3fv(myLoc,1,std::mem::transmute(&col[0]));
+            
+        }
+    }
+    pub fn set_back_color(&mut self,col:[f32;3]){
+        self.back_col=col;
+    }
     pub fn get_dim(&self)->(usize,usize){
         let (width,height)=self.gl_window.get_inner_size().unwrap();
         (width as usize,height as usize)
@@ -299,19 +333,39 @@ impl GlSys{
     pub fn update(&self,verts:&[Vertex]){
         assert!(verts.len()==self.length);
         unsafe{
-            gl::BufferSubData(
-                gl::ARRAY_BUFFER,
-                0,
-                (verts.len()*2 * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                mem::transmute(&verts[0]),
-            );
+            if verts.len()>0{
+                gl::BufferSubData(
+                    gl::ARRAY_BUFFER,
+                    0,
+                    (verts.len()*2 * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                    mem::transmute(&verts[0]),
+                );
+            }
         }
     }
+
+    pub fn re_generate_buffer(&mut self,verts:&[Vertex]){
+        self.length=verts.len();
+        let vbo=&mut self.cs.vbo;
+        unsafe{
+
+            if verts.len()>0{
+                gl::BufferData(
+                    gl::ARRAY_BUFFER,
+                    (verts.len() *2* mem::size_of::<GLfloat>()) as GLsizeiptr,
+                    mem::transmute(&verts[0]),
+                    gl::DYNAMIC_DRAW,
+                );
+            }
+        }
+    }
+    
     pub fn draw(&self){
         use glutin::GlContext;
         unsafe{
+            let b=self.back_col;
             // Clear the screen to black
-            gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+            gl::ClearColor(b[0], b[1], b[2], 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
 
