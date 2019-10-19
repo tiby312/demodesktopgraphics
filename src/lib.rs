a4d97bae35e3b8318e2f76d5af1db5e293d51f37
Ken Reed <kenakioreed@gmail.com>
2019-10-19 10:36:41 -0400

    new api

--- src/lib.rs
+++ src/lib.rs
@@ -67,17 +67,16 @@ struct ContextSetup{
     program:GLuint,
     fs:GLuint,
     vs:GLuint,
-    vbo:u32,
 }
 
 impl Drop for ContextSetup{
     fn drop(&mut self){
+        println!("dropping ");
         // Cleanup
         unsafe {
             gl::DeleteProgram(self.program);
             gl::DeleteShader(self.fs);
             gl::DeleteShader(self.vs);
-            gl::DeleteBuffers(1, &self.vbo);
         }
     }
 }
@@ -86,32 +85,29 @@ impl Drop for ContextSetup{
 
 impl ContextSetup{
 
-    fn new(context:&glutin::Context<PossiblyCurrent>,width:u32,height:u32,verts:&[Vertex],game_world:Rect<f32>,point_size:f32)->ContextSetup{
+    fn new(context:&glutin::Context<PossiblyCurrent>,width:u32,height:u32,game_world:Rect<f32>,point_size:f32)->ContextSetup{
         use glutin::Context;
 
+        println!("yeeee");
         // Load the OpenGL function pointers
         gl::load_with(|symbol| context.get_proc_address(symbol) as *const _);
-
+        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
 
         // Create GLSL shaders
         let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
         let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
         let program = link_program(vs, fs);
 
-        let mut vbo = 0;
-
-        unsafe {
-
-            // Create a Vertex Buffer Object and copy the vertex data to it
-            gl::GenBuffers(1, &mut vbo);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                (verts.len() *mem::size_of::<Vertex>()) as GLsizeiptr,
-                mem::transmute(verts.as_ptr()),
-                gl::DYNAMIC_DRAW,
-            );
-
+        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
+        
+        
+        unsafe{
             // Use shader program
             gl::UseProgram(program);
             
@@ -129,10 +125,11 @@ impl ContextSetup{
                 ptr::null(),
             );
         }
-        
-        Self::set_border_radius(program,game_world,width as usize,height as usize,point_size);
 
-        ContextSetup{fs,vs,vbo,program}
+        Self::set_border_radius(program,game_world,width as usize,height as usize,point_size);
+        println!("yeeee");
+        
+        ContextSetup{fs,vs,program}
     }
 
     fn set_border_radius(program:GLuint,game_world:Rect<f32>,width:usize,height:usize,point_size:f32){
@@ -152,14 +149,20 @@ impl ContextSetup{
                     [0.0, -scaley,],
                 ];    
             
-            let myloc:GLint = gl::GetUniformLocation(program, CString::new("mmatrix").unwrap().as_ptr());
-            gl::UniformMatrix2fv(myloc, 1, 0,std::mem::transmute(&matrix[0][0]));
 
 
 
             let point_size=point_size*(width/w);
             let myloc:GLint = gl::GetUniformLocation(program, CString::new("point_size").unwrap().as_ptr());
             gl::Uniform1f(myloc,point_size);
+            assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
+            let myloc:GLint = gl::GetUniformLocation(program, CString::new("mmatrix").unwrap().as_ptr());
+            assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
+            gl::UniformMatrix2fv(myloc,1, 0,std::mem::transmute(&matrix[0][0]));
+            assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
         }
     }
 
@@ -168,104 +171,41 @@ impl ContextSetup{
 
 
 pub struct GlSys{
-    buffer:Vec<Vertex>,
     windowed_context:glutin::WindowedContext<PossiblyCurrent>,
     cs:ContextSetup,
-    back_col:[f32;3],
     _p:PhantomData<*mut usize>
 }
 
 
-impl GlSys{
-    pub fn get_num_verticies(&self)->usize{
-        self.buffer.len()
-    }
+pub struct Buffer{
+    vbo:u32,
+    buffer:Vec<Vertex>
+}
 
-
-    ///array should be full of xy pairs
-    ///the orientation:
-    ///0,0 is top left
-    ///width,0 is top right
-    ///0,height is bottom left
-    ///width,height is bottom right
-    pub fn new(events_loop:&glutin::EventsLoop)->GlSys{
-        let num_verticies=0;
-        let mut border=axgeom::Rect::new(0.0,0.0,0.0,0.0);
-        let point_size=0.0;
-        //let radius=game_response.new_game_world.unwrap().1;
-
-
-        let mut buffer=Vec::new();
-        buffer.resize(num_verticies,Vertex([0.0;2]));
-
-        let size = glutin::dpi::PhysicalSize::new(1024., 768.);
-
-        let gl_window = glutin::WindowBuilder::new().with_multitouch()
-            .with_dimensions(glutin::dpi::LogicalSize::from_physical(size, 1.0));
- 
-
-        //we are targeting only opengl 3.0 es. and glsl 300 es.
-        
-        let windowed_context = glutin::ContextBuilder::new()
-        .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGlEs, (3, 0)))
-        .with_vsync(true).
-        build_windowed(gl_window,&events_loop).unwrap();
-  
-        let windowed_context = unsafe { windowed_context.make_current().unwrap() };
-
-
-        let glutin::dpi::LogicalSize{width: _,height: _}=windowed_context.window().get_inner_size().unwrap();
-
-        use glutin::Context;
-        
-        let windowed_context = unsafe { windowed_context.make_current() }.unwrap();
-
-        let glutin::dpi::LogicalSize{width,height}=windowed_context.window().get_inner_size().unwrap();
-         // It is essential to make the context current before calling `gl::load_with`.
-        
-        
-        let cs=ContextSetup::new(windowed_context.context(),width as u32,height as u32,&buffer,border,point_size);
-
-        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
-        GlSys{windowed_context,buffer,cs,back_col:[0.,0.,0.],_p:PhantomData}
-
-    }
-    
-
-    pub fn set_bot_color(&mut self,col:[f32;3]){
+impl Drop for Buffer{
+    fn drop(&mut self){
+        //TODO make sure this is ok to do
         unsafe{
-            let myloc:GLint = gl::GetUniformLocation(self.cs.program, CString::new("bcol").unwrap().as_ptr());
-      
-            gl::Uniform3fv(myloc,1,std::mem::transmute(&col[0]));
-            
+            gl::DeleteBuffers(1, &self.vbo);
         }
-        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
-        
     }
+}
 
-    pub fn set_camera_and_bot_radius(&mut self,border:Rect<f32>,radius:f32){
-        let (width,height)=self.get_dim();
 
-        ContextSetup::set_border_radius(self.cs.program,border,width,height,radius);
-        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
-        
-    }
-    pub fn set_back_color(&mut self,col:[f32;3]){
-        self.back_col=col;
-    }
-    pub fn get_dim(&self)->(usize,usize){
-        let glutin::dpi::LogicalSize{width,height}=self.windowed_context.window().get_inner_size().unwrap();
-        (width as usize,height as usize)
-    }
+impl Buffer{
 
-    
     pub fn update<T>(&mut self,arr:&[T],mut func:impl FnMut(&T)->Vertex){
         assert!(arr.len()==self.buffer.len());
         
         for (a,b) in self.buffer.iter_mut().zip(arr.iter()){
             *a=func(b);
         }
+
+        //dbg!(&self.buffer[..20]);
+        
         unsafe{
+            //gl::BindBuffer(gl::ARRAY_BUFFER, self.vbo);
+            //println!("YOOOO");
             gl::BufferSubData(
                 gl::ARRAY_BUFFER,
                 0,
@@ -274,40 +214,203 @@ impl GlSys{
             );
         
         }
-        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
-        
+        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);   
     }
 
-    pub fn re_generate_buffer(&mut self,num_verticies:usize){
-        self.buffer.resize(num_verticies,Vertex([0.0;2]));
-        let _vbo=&mut self.cs.vbo;
+    pub fn get_num_verticies(&self)->usize{
+        self.buffer.len()
+    }
+}
+
+
+impl GlSys{
+    
+    pub fn re_generate_buffer(&mut self,buffer:&mut Buffer,num_verticies:usize){
+        buffer.buffer.resize(num_verticies,Vertex([1000.0;2]));
+        let _vbo=&mut buffer.vbo;
         unsafe{
 
             gl::BufferData(
                 gl::ARRAY_BUFFER,
-                (self.buffer.len() *mem::size_of::<Vertex>()) as GLsizeiptr,
-                mem::transmute(self.buffer.as_ptr()),
+                (buffer.buffer.len() *mem::size_of::<Vertex>()) as GLsizeiptr,
+                mem::transmute(buffer.buffer.as_ptr()),
                 gl::DYNAMIC_DRAW,
             );
         }
         assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
         
     }
-    
-    pub fn draw(&self){
-        use glutin::Context;
-        unsafe{
-            let b=self.back_col;
-            // Clear the screen to black
-            gl::ClearColor(b[0], b[1], b[2], 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            gl::DrawArrays(gl::POINTS,0, self.buffer.len() as i32);
+    pub fn create_vbo(&mut self,num_verticies:usize)->Buffer{
+        let mut vbo = 0;
+        
+        let mut buffer=Vec::new();
+        buffer.resize(num_verticies,Vertex([-1000.0;2]));
+        
+        unsafe {
+
+            // Create a Vertex Buffer Object and copy the vertex data to it
+            gl::GenBuffers(1, &mut vbo);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (buffer.len() *mem::size_of::<Vertex>()) as GLsizeiptr,
+                mem::transmute(buffer.as_ptr()),
+                gl::DYNAMIC_DRAW,
+            );
+
+            
         }
-        self.windowed_context.swap_buffers().unwrap();
+
+        Buffer{vbo,buffer}
+    }
+    ///array should be full of xy pairs
+    ///the orientation:
+    ///0,0 is top left
+    ///width,0 is top right
+    ///0,height is bottom left
+    ///width,height is bottom right
+    pub fn new(events_loop:&glutin::EventsLoop)->GlSys{
+
+        let mut border=axgeom::Rect::new(0.0,0.0,0.0,0.0);
+        let point_size=0.0;
+
+        let size = glutin::dpi::PhysicalSize::new(1024., 768.);
+
+        let gl_window = glutin::WindowBuilder::new().with_multitouch()
+            .with_dimensions(glutin::dpi::LogicalSize::from_physical(size, 1.0));
+ 
+        //assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
+        //we are targeting only opengl 3.0 es. and glsl 300 es.
+        
+        let windowed_context = glutin::ContextBuilder::new()
+        .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGlEs, (3, 0)))
+        .with_vsync(true).
+        build_windowed(gl_window,&events_loop).unwrap();
+
+        //assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+          
+        let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
+        //assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
+        let glutin::dpi::LogicalSize{width: _,height: _}=windowed_context.window().get_inner_size().unwrap();
+
+        use glutin::Context;
+        
+        //assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+                
+        let windowed_context = unsafe { windowed_context.make_current() }.unwrap();
+
+        //assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
+
+        let glutin::dpi::LogicalSize{width,height}=windowed_context.window().get_inner_size().unwrap();
+         // It is essential to make the context current before calling `gl::load_with`.
+        
+        //assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        
+        let cs=ContextSetup::new(windowed_context.context(),width as u32,height as u32,border,point_size);
+
+        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+        GlSys{windowed_context,cs,_p:PhantomData}
+
+    }
+    
+
+    pub fn set_camera_and_bot_radius(&mut self,border:Rect<f32>,radius:f32){
+        let (width,height)=self.get_dim();
+
+        ContextSetup::set_border_radius(self.cs.program,border,width,height,radius);
         assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
         
     }
+
+    pub fn get_dim(&self)->(usize,usize){
+        let glutin::dpi::LogicalSize{width,height}=self.windowed_context.window().get_inner_size().unwrap();
+        (width as usize,height as usize)
+    }
+
+
+    pub fn new_draw_session(&mut self,back_color:[f32;3])->DrawSession{
+        unsafe{
+            
+            gl::ClearColor(back_color[0], back_color[1], back_color[2], 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        DrawSession{a:self}
+    }
 }
 
+
+pub struct DrawSession<'a>{
+    a:&'a mut GlSys
+}
+
+impl<'a> DrawSession<'a>{
+
+    pub fn draw_vbo_section(&mut self,buffer:&Buffer,start:usize,end:usize,color:[f32;3]){
+        unsafe{
+            // Clear the screen to black
+            
+            gl::BindBuffer(gl::ARRAY_BUFFER, buffer.vbo);
+            
+            let myloc:GLint = gl::GetUniformLocation(self.a.cs.program, CString::new("bcol").unwrap().as_ptr());
+      
+            gl::Uniform3fv(myloc,1,std::mem::transmute(&color[0]));
+                    
+            assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+
+            /////
+            let pos_attr = gl::GetAttribLocation(self.a.cs.program, CString::new("position").unwrap().as_ptr());
+            gl::EnableVertexAttribArray(pos_attr as GLuint);
+            gl::VertexAttribPointer(
+                pos_attr as GLuint,
+                2,
+                gl::FLOAT,
+                gl::FALSE as GLboolean,
+                0,
+                ptr::null(),
+            );
+            /////
+
+            gl::DrawArrays(gl::POINTS,start as i32, end as i32);
+        }
+    }
+    pub fn draw_vbo(&mut self,buffer:&Buffer,color:[f32;3]){
+         unsafe{
+            // Clear the screen to black
+            
+            gl::BindBuffer(gl::ARRAY_BUFFER, buffer.vbo);
+            
+            let myloc:GLint = gl::GetUniformLocation(self.a.cs.program, CString::new("bcol").unwrap().as_ptr());
+      
+            gl::Uniform3fv(myloc,1,std::mem::transmute(&color[0]));
+                    
+            assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+
+            /////
+            let pos_attr = gl::GetAttribLocation(self.a.cs.program, CString::new("position").unwrap().as_ptr());
+            gl::EnableVertexAttribArray(pos_attr as GLuint);
+            gl::VertexAttribPointer(
+                pos_attr as GLuint,
+                2,
+                gl::FLOAT,
+                gl::FALSE as GLboolean,
+                0,
+                ptr::null(),
+            );
+            /////
+
+            gl::DrawArrays(gl::POINTS,0, buffer.buffer.len() as i32);
+        }
+    }
+
+    pub fn finish(self){
+        self.a.windowed_context.swap_buffers().unwrap();
+        assert_eq!(unsafe{gl::GetError()},gl::NO_ERROR);
+    }
+}
 
